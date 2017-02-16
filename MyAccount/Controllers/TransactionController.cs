e8c6e541a1aa0051254a089078ffb98360c685d3
Guillaume Gas <guillaume@gas28.net>
2017-02-16 12:20:55 +0100

    Added transaction controller

---
+++ MyAccount/Controllers/TransactionController.cs
@@ -0,0 +1,72 @@
+﻿using MyAccount.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAccount.Controllers
+{
+    [Authorize]
+    public class TransactionController : MainController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.AccountsList = new SelectList(dal.getAccounts(user.id), "id", "name");
+
+            DateTime current_date = DateTime.Now;
+            DateTime begin_date = new DateTime(current_date.Year, current_date.Month, 1);
+
+            List<Transaction> transactions = new List<Transaction>();
+            foreach (var account in dal.getAccounts(user.id))
+            {
+                transactions.AddRange(dal.getTransactions(account.id, begin_date, Dal.TransacFilter.ALL));
+            }
+            ViewBag.TransactionsList = transactions;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int account_id)
+        {
+            ViewBag.AccountsList = new SelectList(dal.getAccounts(user.id), "id", "name");
+
+            DateTime current_date = DateTime.Now;
+            DateTime begin_date = new DateTime(current_date.Year, current_date.Month, 1);
+
+            ViewBag.TransactionsList = dal.getTransactions(account_id, begin_date, Dal.TransacFilter.ALL);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Transaction transaction)
+        {
+            ViewBag.AccountsList = new SelectList(dal.getAccounts(user.id), "id", "name");
+
+            DateTime current_date = DateTime.Now;
+            DateTime begin_date = new DateTime(current_date.Year, current_date.Month, 1);
+
+            if (ModelState.IsValid)
+            {
+                dal.addTransaction(transaction);
+            }
+
+            if (transaction != null)
+            {
+                ViewBag.TransactionsList = dal.getTransactions(transaction.account_id, begin_date, Dal.TransacFilter.ALL);
+            } else
+            {
+                List<Transaction> transactions = new List<Transaction>();
+                foreach (var account in dal.getAccounts(user.id))
+                {
+                    transactions.AddRange(dal.getTransactions(account.id, begin_date, Dal.TransacFilter.ALL));
+                }
+                ViewBag.TransactionsList = transactions;
+            }
+
+            return View("Index");
+        }
+    }
+}
